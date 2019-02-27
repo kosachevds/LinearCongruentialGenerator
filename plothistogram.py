c4ccc59d2ca856fd26cdd3d79e360049a37e1295
Daniil Kosachev <kos-dan@mail.ru>
2019-02-27 22:18:58 +0300

    Added file for histogram plotting

---
+++ plothistogram.py
@@ -0,0 +1,15 @@
+from os import path
+from matplotlib import pyplot as pp
+
+def main():
+    current_dir = path.dirname(__file__)
+    values_filename = path.join(current_dir, "values.txt")
+    with open(values_filename, "rt") as values_file:
+        values_text = values_file.read()
+    values = [int(chunk) for chunk in values_text.split(",") if chunk]
+    pp.hist(x=values)
+    pp.show()
+
+
+if __name__ == "__main__":
+    main()
