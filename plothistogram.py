2141ad79033f78c1c75bbc52d4b11d43b6721b5b
Даниил Косачев <kdaniil@MCS>
2019-02-28 15:19:50 +0300

    Added info output. Added check for empty line

--- plothistogram.py
+++ plothistogram.py
@@ -5,9 +5,13 @@ from matplotlib import pyplot as pp
 def main():
     current_dir = path.dirname(__file__)
     values_filename = path.join(current_dir, "values.txt")
+    print("Reading...")
     with open(values_filename, "rt") as values_file:
         values_text = values_file.read()
-    values = [int(chunk) for chunk in values_text.split(",") if chunk]
+    print("Converting...")
+    values = [int(chunk) for chunk in values_text.split(",")
+              if chunk and chunk != "\n"]
+    print("Plotting...")
     pp.hist(x=values)
     pp.show()
 
