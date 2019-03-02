b5cdee009016ce8ce488cdded6a599d001074335
Daniil Kosachev <kos-dan@mail.ru>
2019-03-02 21:16:01 +0300

    Added class for sequence generation

--- RandomSequenceGenerator.cs
+++ RandomSequenceGenerator.cs
@@ -1,4 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace RandomVariablesModeling
 {
+    class RandomSequenceGenerator {
+        private static System.Random random = new System.Random();
+        private double lower;
+        private double upper;
 
+        RandomSequenceGenerator(double lower, double upper) {
+            this.lower = lower;
+            this.upper = upper;
+        }
+
+        public virtual IEnumerable<double> GenerateSequence(int size) {
+            return Enumerable.Range(0, size).Select(_ => random.NextDouble());
+        }
+    }
 }
